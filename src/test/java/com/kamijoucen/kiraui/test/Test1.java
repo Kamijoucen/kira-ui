32546032923a291a92a3ed1e7dfb37ec0d6447e8
kamijoucen <kamijoucen@foxmail.com>
2022-05-11 00:37:24 +0800

    init

---
+++ src/test/java/com/kamijoucen/kiraui/test/Test1.java
@@ -0,0 +1,12 @@
+package com.kamijoucen.kiraui.test;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+
+}
